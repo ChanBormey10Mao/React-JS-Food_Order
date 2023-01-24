1bb857687c26a1414e7f1fd8f0d10e14f67219bf
Chan Bormey Mao <chanbormeymao@Chans-MBP.gateway>
2023-01-25 00:40:56 +1100

    added cart modal

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart"
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
+  // const [cartIsShown, setCartIsShown] = useState(false);
 
-  function showCartHandler() {
-    setCartIsShown(true);
-  }
-  function hideCardHandler() {
-    setCartIsShown(false);
-  }
+  // function showCartHandler() {
+    
+  //   setCartIsShown(true);
+  // }
+  // function hideCardHandler() {
+  //   setCartIsShown(false);
+  // }
   return (
     <CartProvider>
-      {cartIsShown && <div>Cart...</div>}
-      <Header onShowCart={showCartHandler} />
+      <Cart/>
+      <Header />
       <main>
         <Meals />
       </main>
